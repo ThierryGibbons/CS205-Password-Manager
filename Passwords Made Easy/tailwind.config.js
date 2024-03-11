5c38c96b8f84b60ac289f3305471cf1ebfb5eec5
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-11 21:33:37 +1300

    build tailwindcss

---
+++ Passwords Made Easy/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'text': '#e5ebf3',
+      'background': '#0b1017',
+      'primary': '#a6b5d5',
+      'secondary': '#5e3169',
+      'accent': '#b8699f',
+     },
+    extend: {},
+  },
+  plugins: [],
+}
+
