234812f371b3519c867e1d7e93ec3ddd59782055
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-14 09:00:39 +1300

    feat: imported fonts

--- Passwords Made Easy/tailwind.config.js
+++ Passwords Made Easy/tailwind.config.js
@@ -12,6 +12,23 @@ export default {
       'secondary': '#5e3169',
       'accent': '#b8699f',
      },
+     fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
+    },
+    fontFamily: {
+      heading: 'Poppins',
+      body: 'Poppins',
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },
     extend: {},
   },
   plugins: [],
