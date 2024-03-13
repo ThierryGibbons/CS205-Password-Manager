726fefda3c1faa7752ec72898ded1ea94a2f68e8
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-14 12:05:18 +1300

    feat(HomePage): Basic design of homepage to match UX Design

--- Passwords Made Easy/src/pages/index.tsx
+++ Passwords Made Easy/src/pages/index.tsx
@@ -1,8 +1,28 @@
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div
+        className="grid grid-rows-6 grid-flow-col"
+        style={{ height: `calc(100vh - 192px)` }}
+      >
+        <div className="grid grid-rows-subgrid row-span-3">
+          <h1 className="font-poppins font-extraBold text-3xl text-center row-start-3">
+            MAKING{" "}
+            <span className="inline-block bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent">
+              PASSWORDS
+            </span>{" "}
+            EASY
+            <br />
+            FOR EVERYDAY PEOPLE
+            <br />
+            <span className="font-thin italic">Thats Us..</span>
+          </h1>
+        </div>
+        <h1 className="text-xl text-center mt-[50%] row-start-4 whitespace-pre font-poppins font-medium text-primary">
+          {">>>        Get Started        <<<"}
+        </h1>
+      </div>
+    </>
   );
 };
 
