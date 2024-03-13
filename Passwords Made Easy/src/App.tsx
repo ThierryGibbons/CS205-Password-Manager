d2a35a2c2638190a24385ea24e625459b3ee547b
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-14 12:04:24 +1300

    fix(Navbar): fixed size issues

--- Passwords Made Easy/src/App.tsx
+++ Passwords Made Easy/src/App.tsx
@@ -10,31 +10,14 @@ import BlogPage from "./pages/blog";
 import AccountPage from "./pages/account";
 
 function App() {
-  const [navbarHeight, setNavbarHeight] = useState(0);
-  const navbarRef = useRef(null); // Create a ref for the Navbar
-
-  useEffect(() => {
-    // Measure the height of the Navbar after the component mounts
-    console.log("before?: ", navbarRef.current); // Check if the ref is attached
-    if (navbarRef.current) {
-      console.log("something: ", navbarRef.current.offsetHeight); // Check the measured height
-      setNavbarHeight(navbarRef.current.offsetHeight); // This sets the navbar's height
-    }
-  }, []); // Empty dependency array ensures this runs once after initial render
-
-  console.log("navbar height: ", navbarHeight); // Check the state update
-
   return (
-    <>
+    <div className="bg-background">
       <>
         <div className="App flex flex-col h-screen">
-          <div ref={navbarRef}>
+          <div className="h-24">
             <Navbar />
           </div>
-          <div
-            style={{ paddingBottom: `${navbarHeight}px` }}
-            className="flex-1 flex items-center justify-center"
-          >
+          <div className="flex-1 flex items-center justify-center">
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/blog" element={<BlogPage />} />
@@ -43,7 +26,7 @@ function App() {
           </div>
         </div>
       </>
-    </>
+    </div>
   );
 }
 
