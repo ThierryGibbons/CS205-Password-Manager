3a21d35f7edc48ae85859fd44eb4cb407599a075
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-14 12:03:10 +1300

    style(tailwindcss): added more stuff

--- Passwords Made Easy/tailwind.config.js
+++ Passwords Made Easy/tailwind.config.js
@@ -11,6 +11,7 @@ export default {
       'primary': '#a6b5d5',
       'secondary': '#5e3169',
       'accent': '#b8699f',
+      'transparent': 'transparent',
      },
      fontSize: {
       sm: '0.750rem',
@@ -29,7 +30,9 @@ export default {
       thin: '200',
       light: '300',
       normal: '400',
+      medium: '500',
       bold: '700',
+      extraBold: '1000',
     },
     extend: {},
   },
