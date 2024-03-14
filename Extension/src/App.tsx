825f6e4fca310f173f1a9cda9bbca70ae5f9b1ed
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-15 12:40:42 +1300

    init(setup): setup and some basic ui navbar

---
+++ Extension/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import NavBar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <NavBar v="Password" />
+      <h1 className="font-Poppins font-bold p-18">Passwords Made Easy</h1>
+    </>
+  );
+}
+
+export default App;
