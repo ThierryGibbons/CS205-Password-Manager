8c6f895a1815f27ccade98f4d5f82c07b04c3919
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-20 17:52:49 +1300

    feat(PasswordView): Basic layout initialised

--- Extension/src/pages/password.tsx
+++ Extension/src/pages/password.tsx
@@ -2,6 +2,43 @@ const PasswordPage = () => {
   return (
     <div>
       <h1 className="font-Poppins font-bold p-18">Password Page</h1>
+      {/* List of Sites on left and Site URLS on right */}
+      <div className="flex flex-row justify-center">
+        <div className="flex flex-col">
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[30vw] items-center">
+            Site
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[30vw] items-center">
+            Site
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[30vw] items-center">
+            Site
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[30vw] items-center">
+            Site
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[30vw] items-center">
+            Site
+          </h2>
+        </div>
+        <div className="flex flex-col">
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[60vw] items-center">
+            https://website.com
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[60vw] items-center">
+            https://website.com
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[60vw] items-center">
+            https://website.com
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[60vw] items-center">
+            https://website.com
+          </h2>
+          <h2 className="font-Poppins font-bold bg-background-900 p-3 m-2 rounded-lg w-[60vw] items-center">
+            https://website.com
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
