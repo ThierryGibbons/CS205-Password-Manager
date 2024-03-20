cc7b3e7db933fd5f33ca72adab1c5bc2a053a497
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-20 16:43:03 +1300

    refactor(UI Bug): Changes following review of this bug fix

--- Passwords Made Easy/src/pages/index.tsx
+++ Passwords Made Easy/src/pages/index.tsx
@@ -7,9 +7,7 @@ const HomePage = () => {
         className="grid grid-rows-6 grid-flow-col"
         style={{ height: `calc(100vh - 192px)` }}
       >
-        <div></div>
-        <div></div>
-        <div className="row-span-2" style={{ width: "80ch", margin: "0 auto" }}>
+        <div className="row-span-2 w-[80ch] m-auto">
           <h1 className="font-poppins font-extraBold text-3xl text-center">
             <InteractiveText originalText="MAKING" />{" "}
             <span className="inline-block bg-gradient-to-r from-primary-default to-accent-default bg-clip-text text-transparent">
@@ -23,12 +21,13 @@ const HomePage = () => {
             <br />
             <span className="font-thin italic">
               <InteractiveText originalText="Thats Us"></InteractiveText>
+              {".."}
             </span>
           </h1>
         </div>
         <div className="flex flex-col justify-center">
           <h1 className="text-xl text-center whitespace-pre font-poppins font-medium text-primary-default">
-            <a href="/getstarted">{">>> Get Started <<<"}</a>
+            <a href="/getstarted">{">>>        Get Started        <<<"}</a>
           </h1>
         </div>
       </div>
