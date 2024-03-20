82ac02c9f32ed9d1796b2d16631deba0c4936b78
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-20 13:32:34 +1300

    feat(Generate): Checks password quality and will auto regenerate if not substantial

--- Extension/src/components/Generate.tsx
+++ Extension/src/components/Generate.tsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 
+const checkPasswordQuality = (password: string): boolean => {
+  // Check if the password meets the required criteria
+  const hasUpperCase = /[A-Z]/.test(password);
+  const hasLowerCase = /[a-z]/.test(password);
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+{}[\]|:;<>,.?/`~]/.test(password);
+  const isLongEnough = password.length >= 8;
+
+  // Return true if all criteria are met, otherwise return false
+  return (
+    hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && isLongEnough
+  );
+};
+
 const Generate = () => {
   const [text, setText] = useState("Generate");
   const generateRandomString = () => {
@@ -14,9 +28,21 @@ const Generate = () => {
   };
 
   const handleClick = () => {
-    const randomString = generateRandomString();
-    setText(randomString);
-    console.log(randomString);
+    const run = true;
+    while (run) {
+      console.log("Generating random string...");
+      const randomString = generateRandomString();
+      if (checkPasswordQuality(randomString)) {
+        setText(randomString);
+        console.log(
+          "Requirements Met\nrandomString:",
+          randomString,
+          "length: ",
+          randomString.length
+        );
+        break;
+      }
+    }
   };
 
   return (
