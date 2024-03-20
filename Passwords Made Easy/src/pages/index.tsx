4920cfa638cb9fe9c8329737c38120f78c88769a
Oliver Eddy <olivereddy.developer@gmail.com>
2024-03-20 15:01:56 +1300

    Bug fix:
    Set `80ch` width for `InteractiveText` to fix layout shifts in dynamic text, aligning content in designated rows to retain grid layout.

--- Passwords Made Easy/src/pages/index.tsx
+++ Passwords Made Easy/src/pages/index.tsx
@@ -7,8 +7,10 @@ const HomePage = () => {
         className="grid grid-rows-6 grid-flow-col"
         style={{ height: `calc(100vh - 192px)` }}
       >
-        <div className="grid grid-rows-subgrid row-span-3">
-          <h1 className="font-poppins font-extraBold text-3xl text-center row-start-3">
+        <div></div>
+        <div></div>
+        <div className="row-span-2" style={{ width: "80ch", margin: "0 auto" }}>
+          <h1 className="font-poppins font-extraBold text-3xl text-center">
             <InteractiveText originalText="MAKING" />{" "}
             <span className="inline-block bg-gradient-to-r from-primary-default to-accent-default bg-clip-text text-transparent">
               <InteractiveText originalText="PASSWORDS" />
@@ -21,13 +23,14 @@ const HomePage = () => {
             <br />
             <span className="font-thin italic">
               <InteractiveText originalText="Thats Us"></InteractiveText>
-              {".."}
             </span>
           </h1>
         </div>
-        <h1 className="text-xl text-center mt-[50%] row-start-4 whitespace-pre font-poppins font-medium text-primary-default">
-          <a href="/getstarted">{">>>        Get Started        <<<"}</a>
-        </h1>
+        <div className="flex flex-col justify-center">
+          <h1 className="text-xl text-center whitespace-pre font-poppins font-medium text-primary-default">
+            <a href="/getstarted">{">>> Get Started <<<"}</a>
+          </h1>
+        </div>
       </div>
     </>
   );
