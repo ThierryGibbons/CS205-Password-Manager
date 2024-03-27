722bed6711f65aaf8e39f19d6199e173b4989bf2
Thierry <270067227@yoobeestudent.ac.nz>
2024-03-27 14:31:13 +1300

    feat(database): basic database structure

--- Server/app.py
+++ Server/app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db' # Placeholder URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Removes warning
+db = SQLAlchemy(app)
+CORS(app)
+
+class Item(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True) # Gives each user a unique ID
+    email = db.Column(db.String(100))
+
+    def __repr__(self):
+        return '<Item %r>' % self.email
 
 @app.route('/')
 def home():
     return "Hello, World!"
 
+@app.route('/items', methods=['POST'])
+def add_item():
+    email = request.json['email']
+    item = Item(email=email)
+    db.session.add(item)
+    db.session.commit()
+    return jsonify({"success": True, "response": "Item added"}), 201 # 201 is the status code for created
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    item_list = []
+    for item in items:
+        item_list.append({"email": item.email})
+    return jsonify({"success": True, "response": item_list})
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=False)
