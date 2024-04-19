b206acdb780fc2764a71f05db653957b40a07095
Oliver Eddy <olivereddy.developer@gmail.com>
2024-04-19 13:48:50 +1200

    Update get started button to login with redirect,
    Create new element in nav bar to login and log out, button is dynamic

--- Passwords Made Easy/src/components/Navbar.tsx
+++ Passwords Made Easy/src/components/Navbar.tsx
@@ -1,29 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import InteractiveText from "./InteractiveText";
+import { useAuth0 } from '@auth0/auth0-react';
 
-const Navbar: React.FC = () => {
-  return (
-    <>
-      <nav className="p-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <Link to="/" className="text-text-default hover:text-accent-default text-2xl font-bold font-Poppins">
-            <InteractiveText originalText="Passwords" />{" "}
-            <InteractiveText originalText="Made" />{" "}
-            <InteractiveText originalText="Easy" />
-          </Link>
-          <div>
-            <Link to="/blog" className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl">
-              Blog
-            </Link>
-            <Link to="/account" className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl">
-              Account
-            </Link>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+const Navbar = () => {
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    return (
+        <>
+            <nav className="p-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <Link to="/" className="text-text-default hover:text-accent-default text-2xl font-bold font-Poppins">
+                        <InteractiveText originalText="Passwords" />{" "}
+                        <InteractiveText originalText="Made" />{" "}
+                        <InteractiveText originalText="Easy" />
+                    </Link>
+                    <div>
+                        {isAuthenticated ? (
+                            <button
+                                onClick={() => logout({ returnTo: window.location.origin })}
+                                className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl mr-2"
+                            >
+                                Log Out
+                            </button>
+                        ) : (
+                            <button
+                                onClick={() => loginWithRedirect()}
+                                className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl mr-2"
+                            >
+                                Log In
+                            </button>
+                        )}
+                        <Link to="/blog" className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl">
+                            Blog
+                        </Link>
+                        <Link to="/account" className="px-3 py-2 rounded text-text-default hover:text-accent-default font-Poppins font-light text-xl">
+                            Account
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
 };
 
 export default Navbar;
--- Passwords Made Easy/src/pages/index.tsx
+++ Passwords Made Easy/src/pages/index.tsx
@@ -1,46 +1,46 @@
-import InteractiveText from "../components/InteractiveText";
+import React from 'react';
+import InteractiveText from '../components/InteractiveText';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const HomePage = () => {
-  return (
-    <>
-      <div
-        className="grid grid-rows-6 grid-flow-col"
-        style={{ height: `calc(100vh - 192px)` }}
-      >
-        <div className="row-span-2 w-[80ch] m-auto row-start-2">
-          <h1 className="font-poppins font-extraBold text-3xl text-center">
-            <InteractiveText originalText="MAKING" />{" "}
-            <span className="inline-block bg-gradient-to-r from-primary-default to-accent-default bg-clip-text text-transparent">
-              <InteractiveText originalText="PASSWORDS" />
-            </span>{" "}
-            <InteractiveText originalText="EASY" />
-            <br />
-            <InteractiveText originalText="FOR" />{" "}
-            <InteractiveText originalText="EVERYDAY" />{" "}
-            <InteractiveText originalText="PEOPLE" />
-            <br />
-            <span className="font-thin italic">
-              <InteractiveText originalText="Thats Us"></InteractiveText>
-              {".."}
-            </span>
-          </h1>
-        </div>
+    const { loginWithRedirect } = useAuth0();
 
+    return (
+        <>
+            <div
+                className="grid grid-rows-6 grid-flow-col"
+                style={{ height: `calc(100vh - 192px)` }}
+            >
+                <div className="row-span-2 w-[80ch] m-auto row-start-2">
+                    <h1 className="font-poppins font-extraBold text-3xl text-center">
+                        <InteractiveText originalText="MAKING" />{' '}
+                        <span className="inline-block bg-gradient-to-r from-primary-default to-accent-default bg-clip-text text-transparent">
+                            <InteractiveText originalText="PASSWORDS" />
+                        </span>{' '}
+                        <InteractiveText originalText="EASY" />
+                        <br />
+                        <InteractiveText originalText="FOR" />{' '}
+                        <InteractiveText originalText="EVERYDAY" />{' '}
+                        <InteractiveText originalText="PEOPLE" />
+                        <br />
+                        <span className="font-thin italic">
+                            <InteractiveText originalText="That's Us" />
+                            {'..'}
+                        </span>
+                    </h1>
+                </div>
 
-        <div className="flex flex-col justify-center">
-          <h1 className="text-xl text-center whitespace-pre font-poppins font-medium text-primary-default">
-          <a href={`${import.meta.env.VITE_BACKEND_URL}/login`}>{">>> Get Started <<<"}</a>
-
-          </h1>
-        </div>
-
-
-
-
-      </div>
-    </>
-  );
+                <div className="flex flex-col justify-center">
+                    <button
+                        className="text-xl text-center whitespace-pre font-poppins font-medium text-primary-default cursor-pointer"
+                        onClick={() => loginWithRedirect()}
+                    >
+                        {">>> Get Started <<<"}
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default HomePage;
