5503db138ab920339acd89384da29801055b7b6c
Binary-Rabbit <ellaine.simpkin@gmail.com>
2024-04-20 13:32:08 +1200

    refactor(PwnedApi): moved PwnedApi to Extension

--- Extension/src/components/PwnedApi.tsx
+++ Extension/src/components/PwnedApi.tsx
@@ -3,6 +3,8 @@
 
 const Pwned = (email: string) => {
 
+    let result = "";
+
     fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${email}`, {
         headers: {
             'hibp-api-key': '5dec7a3c1d04424693816febd897cd2a'
@@ -11,11 +13,13 @@ const Pwned = (email: string) => {
         
     }).then(res => {
         if (res.ok){
-            console.log('BREACH DETECTED')
+            result = "Your email address has been the victim of a data breach"
         } else {
-            console.log('A-Okay')
+            result = "Your email is secure"
         }
     })
+
+    return result;
 
 };
 
