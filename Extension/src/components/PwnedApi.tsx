8ab0ba9123dfd500db8a30deab3568c7e1df4f08
Thierry <270067227@yoobeestudent.ac.nz>
2024-04-20 18:06:08 +1200

    style(basic): fixed

--- Extension/src/components/PwnedApi.tsx
+++ Extension/src/components/PwnedApi.tsx
@@ -1,26 +1,20 @@
-
-
-
 const Pwned = (email: string) => {
+  let result = "";
 
-    let result = "";
+  fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${email}`, {
+    headers: {
+      "hibp-api-key": "5dec7a3c1d04424693816febd897cd2a",
+    },
+    method: "GET",
+  }).then((res) => {
+    if (res.ok) {
+      result = "Your email address has been the victim of a data breach";
+    } else {
+      result = "Your email is secure";
+    }
+  });
 
-    fetch(`https://haveibeenpwned.com/api/v3/breachedaccount/${email}`, {
-        headers: {
-            'hibp-api-key': '5dec7a3c1d04424693816febd897cd2a'
-        },
-        method: 'GET',
-        
-    }).then(res => {
-        if (res.ok){
-            result = "Your email address has been the victim of a data breach"
-        } else {
-            result = "Your email is secure"
-        }
-    })
-
-    return result;
-
+  return <h1>{result}</h1>;
 };
 
 export default Pwned;
